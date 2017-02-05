0de1d5893095df02cfc470266a86bbc9bb2f508f
balanus <balaprasannav2009@gmail.com>
2017-02-05 13:29:08 +0800

    string1 solution all passes

--- basic/string1.py
+++ basic/string1.py
@@ -25,8 +25,10 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
   # +++your code here+++
-  return
-
+    donut_count_str = 'Number of donuts: '
+    if count >= 10:
+        return donut_count_str + 'many'
+    return donut_count_str + str(count)
 
 # B. both_ends
 # Given a string s, return a string made of the first 2
@@ -35,7 +37,9 @@ def donuts(count):
 # is less than 2, return instead the empty string.
 def both_ends(s):
   # +++your code here+++
-  return
+    if len(s) < 2:
+        return ''
+    return s[:2] +s[-2:]
 
 
 # C. fix_start
@@ -49,7 +53,7 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
   # +++your code here+++
-  return
+  return s[0] + s.replace(s[0],'*')[1:]
 
 
 # D. MixUp
@@ -61,7 +65,7 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  return b[:2]+a[2:] +' '+ a[:2]+b[2:] 
 
 
 # Provided simple test() function used in main() to print
